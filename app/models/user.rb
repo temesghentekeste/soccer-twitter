ae7fd0cb462e582deb007ddd3a69b593bd1a3a5e
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-12-01 13:16:20 +0300

    Amend following model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,21 @@ class User < ApplicationRecord
     
   has_many :opinions, foreign_key: "author_id", dependent: :destroy
 
-  has_many :active_Followings, class_name: "Following", foreign_key:    "follower_id", dependent: :destroy
-  has_many :passive_Followings, class_name: "Following", foreign_key:    "followed_id", dependent: :destroy
+  has_many :active_followings, class_name: "Following", foreign_key:    "follower_id", dependent: :destroy
+  has_many :passive_followings, class_name: "Following", foreign_key:    "followed_id", dependent: :destroy
 
-  has_many :following, through: :active_Followings, source: :followed
-  has_many :followers, through: :passive_Followings, source: :follower
+  has_many :following, through: :active_followings, source: :followed
+  has_many :followers, through: :passive_followings, source: :follower
+
+  def follow(other)
+    active_followings.create!(followed_id: other.id)
+  end
+
+  def unfollow(other)
+    active_followings.find_by(followed_id: other.id).destroy
+  end
+
+  def following?(other)
+    following.include?(other)
+  end
 end
