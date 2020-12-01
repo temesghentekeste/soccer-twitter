2e490128aab5cdae289fb2bcb5a30be9260d8b7e
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-12-01 13:35:59 +0300

    Add validations to users and opinions

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 105 },
+                    uniqueness: { case_sensitive: false },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :full_name, presence: true, length: { maximum: 50 }
+       
+
   has_one_attached :photo, dependent: :destroy
   has_one_attached :cover_image, dependent: :destroy
     
