ebb4d368abf59436373d8a3179f04013ea143f02
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-12-01 13:03:01 +0300

    - Add migration to create followings table - Add users controller, add users index, show views - Add registrations controller - Amend user-opinion relationships - Update devise registrations new and edit views - Update routes to reflect the above changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,11 @@ class User < ApplicationRecord
   has_one_attached :photo, dependent: :destroy
   has_one_attached :cover_image, dependent: :destroy
     
-  has_many :opinions
+  has_many :opinions, foreign_key: "author_id", dependent: :destroy
+
+  has_many :active_Followings, class_name: "Following", foreign_key:    "follower_id", dependent: :destroy
+  has_many :passive_Followings, class_name: "Following", foreign_key:    "followed_id", dependent: :destroy
+
+  has_many :following, through: :active_Followings, source: :followed
+  has_many :followers, through: :passive_Followings, source: :follower
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations'}
+
   root to: "home#index"
+
+  resources :users, only: [:index, :show]
 end
