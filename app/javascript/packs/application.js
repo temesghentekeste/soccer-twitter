2f90ef2558d507c56c807064e8a13b7991b69392
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-12-02 11:44:35 +0300

    Add bootstrap, jquery and popper; add google fonts

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,11 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+require("bootstrap")
+import "../stylesheets/application";
+document.addEventListener("turbolinks:load", function() {
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+        $('[data-toggle="popover"]').popover()
+    })
+})
