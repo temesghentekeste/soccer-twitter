84649bb782cea7fad2d9804d186ba4d3034ef5c8
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-12-03 22:53:42 +0300

    Allow signed in users to create a soccer tweet, refactor home and index views using partial views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,22 @@
 class PagesController < ApplicationController
-  def index 
+  before_action :authenticate_user!, except: %i[index]
+  before_action :set_initial_values
 
+
+  def index 
+    
   end
 
   def home
-    @opinion = Opinion.new
-    @opinions = current_user.opinions.includes(:author).limit(10)
-    @to_follow = User.all - current_user.following
+    
+  end
+
+  private 
+  def set_initial_values
+    if user_signed_in?
+      @opinion = Opinion.new
+      @opinions = current_user.opinions.includes(:author).limit(10)
+      @to_follow = User.all - current_user.following
+    end
   end
 end
