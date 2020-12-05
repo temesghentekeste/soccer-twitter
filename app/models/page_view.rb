90e918641bc3a62d54915dcdf9bb5e2b1637d12f
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-12-05 14:12:22 +0300

    Add scopes to retrieve different stats of a profile page

--- app/models/page_view.rb
+++ app/models/page_view.rb
@@ -1,5 +1,17 @@
 class PageView < Impression
   scope :for_type, -> (type) do
-    Impression.where(impressionable_type: type)
+    where(impressionable_type: type)
+  end
+  
+  scope :for_date_range, -> (start_date, end_date) do
+    where(created_at: start_date.to_date.beginning_of_day..end_date.to_date.end_of_day)
+  end
+
+  scope :count_by_date, -> () do
+    group('date(created_at)').count
+  end
+
+  scope :uniq_count_by_date, -> () do
+    select(:ip_address).distinct.group('date(created_at)').count
   end
 end
